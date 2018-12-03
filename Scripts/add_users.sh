c2b72897bf2a6a394e9a2345852389034a74a605
packituz-gttz <pacohm94@gmail.com>
2018-12-03 15:06:20 -0600

    Fixed Typos

--- Scripts/add_users.sh
+++ Scripts/add_users.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+host="localhost"
+port=8888
 input="users_me.csv"
 while IFS= read -r var
 do
-  name="$(echo "$var" | cut -d "," -f 1)"
-  echo "$name"
-  echo "{\"data\": {\"password\": \"$name\"}}"
-  #echo "{\"data\": {\"password\": \"$name\"}}" | http PUT http://ec2-18-232-250-82.compute-1.amazonaws.com:8888/v1/accounts/$name --verbose
-  echo "{\"data\": {\"password\": \"$name\"}}" | http PUT http://localhost:8888/v1/accounts/$name --verbose
+ name="$(echo "$var" | cut -d "," -f 1)"
+ pass="$(echo "$var" | cut -d "," -f 2)"
+ echo "$name"
+ echo "{\"data\": {\"password\": \"$pass\"}}"
+ echo "{\"data\": {\"password\": \"$pass\"}}" | http PUT http://$host:$port/v1/accounts/$name --verbose
 done < "$input"
-
